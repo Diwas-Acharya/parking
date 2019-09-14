36368d7a9183664407acab09cbf5f88580e5765e
Diwas <diwasacharya169@gmail.com>
2019-09-14 15:21:12 +0530

    adding all

---
+++ project form valid.js
@@ -0,0 +1,15 @@
+function ok(){
+	var x=document.getElementById('uname');
+	var y=document.getElementById('pname');
+	if(x.value=="" || y.value=="")
+	{
+		alert("Please Enter correct Input");
+		return false;
+	}
+	
+	return true;
+}
+
+
+
+document.getElementById("li").addEventListener("mouseover", function li(){alert("hello");};
